84ee49b5acea30db9cc8b5b5be5932ea3def36ff
adsoftsito <adsoft@live.com.mx>
2025-03-11 00:49:32 -0600

    Update people.py

--- people.py
+++ people.py
@@ -30,13 +30,13 @@ if __name__ == "__main__":
     #print(results)
     df_people_1903_1906.write.mode("overwrite").json("results")
     #df_people_1903_1906.coalesce(1).write.json('results/data_merged.json')
-    with open('results/data.json', 'w') as file:
-        json.dump(results, file)
+    #with open('results/data.json', 'w') as file:
+    #    json.dump(results, file)
 
     
     # Example DataFrame
     
-    #df = spark.read.json("results/data.json")
+    df = spark.read.json("results/data.json")
 
     # Convert DataFrame columns to a "value" column that contains data in a JSON format
     #df = df.select(to_json(struct("*")).alias("value"))
@@ -47,13 +47,13 @@ if __name__ == "__main__":
     ])
 
 # Parse the JSON string into a Struct type 
-    #df = df.select(from_json(df.toJSON(), schema).alias("value"))
+    df = df.select(from_json(df.value, schema).alias("value"))
     # Write DataFrame to Kafka topic
-    #df.selectExpr("CAST(value AS STRING)").write \
-    #    .format("kafka") \
-    #    .option("kafka.bootstrap.servers", "localhost:9092") \
-    #    .option("topic", "adsoft") \
-    #    .save()
+    df.selectExpr("CAST(value AS STRING)").write \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "adsoft") \
+        .save()
     
     #query='SELECT sex,COUNT(sex) FROM people WHERE birth BETWEEN "1903-01-01" AND "1911-12-31" GROUP BY sex'
     #df_people_1903_1906_sex = spark.sql(query)
