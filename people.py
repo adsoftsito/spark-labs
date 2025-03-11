38124808e47b83ac6784f5972d41309ec557294d
adsoftsito <adsoft@live.com.mx>
2025-03-10 23:24:26 -0600

    Update people.py

--- people.py
+++ people.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     df_people_names = spark.sql(query)
     df_people_names.show(20)
 
-    query='SELECT name, `birth` FROM people WHERE `birth` BETWEEN "1903-01-01" AND "1950-12-31" ORDER BY `birth`'
+    query='SELECT name as key, `birth` as value FROM people WHERE `birth` BETWEEN "1903-01-01" AND "1915-12-31" ORDER BY `birth`'
     df_people_1903_1906 = spark.sql(query)
     df_people_1903_1906.show(20)
     results = df_people_1903_1906.toJSON().collect()
@@ -29,7 +29,21 @@ if __name__ == "__main__":
     with open('results/data.json', 'w') as file:
         json.dump(results, file)
 
-    query='SELECT sex,COUNT(sex) FROM people WHERE birth BETWEEN "1903-01-01" AND "1911-12-31" GROUP BY sex'
-    df_people_1903_1906_sex = spark.sql(query)
-    df_people_1903_1906_sex.show()
+    
+    # Example DataFrame
+    df = spark.read.json("path_to_your_json_file")
+
+    # Convert DataFrame columns to a "value" column that contains data in a JSON format
+    df = df.select(to_json(struct("*")).alias("value"))
+
+    # Write DataFrame to Kafka topic
+    df.selectExpr("CAST(value AS STRING)").write \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "adsoft") \
+        .save()
+    
+    #query='SELECT sex,COUNT(sex) FROM people WHERE birth BETWEEN "1903-01-01" AND "1911-12-31" GROUP BY sex'
+    #df_people_1903_1906_sex = spark.sql(query)
+    #df_people_1903_1906_sex.show()
     spark.stop()
